f76b5d6e355c8e8ad4e331c61154b0e379fbba58
William Grundler <wrgrundler@gmail.com>
2020-04-27 23:54:05 -0700

    question index equals answer index

--- script2.js
+++ script2.js
@@ -2,6 +2,7 @@ var time=document.querySelector("#time");
 var startButton=document.querySelector("#start");
 var count=localStorage.getItem("count");
 var question = document.getElementById("question");
+/*var userAnswers = [];*/
 
 var secondsLeft = 60;
 
@@ -26,98 +27,54 @@ function setTime() {
     }, 1000);
 }
 
-    // The array of questions for our quiz game.
-    /*var questions = [
-        { q: "The sky is blue." a: "t" },
-        { q: "There are 365 days in a year.", a: "t" },
-        { q: "There are 42 ounces in a pound.", a: "f" },
-        { q: "The Declaration of Independence was created in 1745.", a: "f" },
-        { q: "Bananas are vegetables.", a: "f" }
-      ];*/
+var questions = [
+    { q: "The sky is blue.", a: "t" },
+    { q: "There are 365 days in a year.", a: "t" },
+    { q: "There are 42 ounces in a pound.", a: "f" },
+    { q: "The Declaration of Independence was created in 1745.", a: "f" },
+    { q: "Bananas are vegetables.", a: "f" }
+  ];
 
-      var questions = [
+  var answers = [
+      {a:"1", c:"t"},
+      {a: "2", c:"t"},
+      {a: "3", c:"f"},
+      {a: "4", c: "f"},
+      {a: "5", c: "f"},
+  ];
 
+  console.log(answers[2].c)
 
-            {
-        
-                q : "What does HTML stand for?",
-        
-                choiceA : "Correct",
-        
-                choiceB : "Wrong",
-        
-                choiceC : "Wrong",
-        
-                correct : "A"
-        
-            },{
-        
-                q : "What does CSS stand for?",
-        
-                choiceA : "Wrong",
-        
-                choiceB : "Correct",
-        
-                choiceC : "Wrong",
-        
-                correct : "B"
-        
-            },{
-        
-                q : "What does JS stand for?",
-        
-                choiceA : "Wrong",
-        
-                choiceB : "Wrong",
-        
-                choiceC : "Correct",
-        
-                correct : "C"
-        
-            }
-        
-        ];
+  // We start the game with a score of 0.
+  var score = 0;
 
-        function renderQuestion(){
+  // Loop over every question object
+  for (var i = 0; i < questions.length; i++) {
+    // Display current question to user and ask OK/Cancel
+    var answer = confirm(questions[i].q);
 
-            let q = questions[runningQuestion];
-        
-           
-        
-            question.innerHTML = "<p>"+ q.question +"</p>";
-        
-            
-        
-            choiceA.innerHTML = q.choiceA;
-        
-            choiceB.innerHTML = q.choiceB;
-        
-            choiceC.innerHTML = q.choiceC;
-        
-        }
+    // Compare answers
+    if (((answer === true && (questions[i].a === answers[i].c)) ||
+        (answer === false && (questions[i].a === answers[i].c)))){
+   // if ((answer === true && questions[i].a === "t") ||
+   //   (answer === false && questions[i].a === "f")) {
+      // Increase score
+      score++;
+      alert("Correct!");
+    }
+    else {
+      alert("Wrong!");
+    }
+  }
 
-        
-  
-      // We start the game with a score of 0.
-      var score = 0;
-  
-      // Loop over every question object
-      for (var i = 0; i < questions.length; i++) {
-        // Display current question to user and ask OK/Cancel
-       // var answer = confirm(questions[i].q);
-  
-        // Compare answers
-        if ((answer === true && questions[i].a === "t") ||
-          (answer === false && questions[i].a === "f")) {
-          // Increase score
-          score++;
-          alert("Correct!");
-        }
-        else {
-          alert("Wrong!");
-        }
-      }
-  
-      // Show total at end
-      alert("You got " + score + "/" + questions.length);
+  // Show total at end
+  alert("You got " + score + "/" + questions.length);
+
+ /* var submitAnswer = function() {
+    var answer = document.getElementById(submitq1);
+    userAnswers.push[answer];
+ };*/
+
+ //submitButton.addEventListener("click", function(){
+  //   if (answer === true && questions[i].a === answers[i].t)
   
